c90eab2f26055de608d99887fed58b217b94c791
Michael Verhaeghe <michael.verhaeghe.v3967@student.hogent.be>
2018-01-17 17:07:28 +0100

    CRUD tools uitbreiding.

--- webapps-opdracht/src/app/edit-image/edit-image.component.ts
+++ webapps-opdracht/src/app/edit-image/edit-image.component.ts
@@ -1,3 +1,7 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Image } from '../image/image.model';
+import { Observable } from 'rxjs/Rx';
+import { ImageService } from '../image.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditImageComponent implements OnInit {
 
-  constructor() { }
+  private imageService: ImageService;
+  private imagesList: Observable<Image[]>;
+  editForm: FormGroup;
+
+  constructor(imageService: ImageService, private fb: FormBuilder) {
+
+    this.editForm = fb.group({
+      'image': [null, Validators.required],
+      'gallery': [null, Validators.required],
+      'description': [null, Validators.required],
+      'url': [null, Validators.required]
+    });
+
+    this.imageService = imageService;
+    this.imagesList = imageService.images;
+  }
+
+  saveImage(image) {
+    // TODO: Save
+    console.log('testSave');
+  }
+
 
   ngOnInit() {
   }
