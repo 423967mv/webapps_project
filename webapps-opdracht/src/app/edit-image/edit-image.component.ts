1982ed50e85b5dd5b14a94ae18fb910a2b148a40
Michael <michael_verhaeghe@hotmail.com>
2018-08-12 17:26:36 +0200

    Edit image form autocomplete works.

--- webapps-opdracht/src/app/edit-image/edit-image.component.ts
+++ webapps-opdracht/src/app/edit-image/edit-image.component.ts
@@ -12,16 +12,25 @@ import { Component, OnInit } from '@angular/core';
 export class EditImageComponent implements OnInit {
 
   private imagesList: Observable<Image[]>;
+  private selected = {
+    _id: '',
+    _title: '',
+    _gallery: '',
+    _description: '',
+    _url: ''
+
+  };
   editForm: FormGroup;
 
   constructor(private imageService: ImageService, private fb: FormBuilder) {
 
+    // Message doesn't appear with minLength(1)
     this.editForm = fb.group({
-      'title': [null, Validators.required],
-      'id': [null, Validators.required],
-      'gallery': [null, Validators.required],
-      'description': [null, Validators.required],
-      'url': [null, Validators.required]
+      'title': [this.selected._title, Validators.minLength(2)],
+      'id': [this.selected._id, Validators.minLength(2)],
+      'gallery': [this.selected._gallery, Validators.minLength(2)],
+      'description': [this.selected._description, Validators.minLength(2)],
+      'url': [this.selected._url, Validators.minLength(2)]
     });
 
     this.imagesList = imageService.images;
