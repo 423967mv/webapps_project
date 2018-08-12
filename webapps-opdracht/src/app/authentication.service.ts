f28c9ab16fcdb69ae355a2611f4060c2cf0a5def
Michael <michael_verhaeghe@hotmail.com>
2018-08-12 22:09:52 +0200

    Absolute url for authenticationService.

--- webapps-opdracht/src/app/authentication.service.ts
+++ webapps-opdracht/src/app/authentication.service.ts
@@ -5,8 +5,8 @@ import { Router } from '@angular/router';
 @Injectable()
 export class AuthenticationService {
 
-  private _registerUrl = '/API/users/register';
-  private _loginUrl = '/API/users/login';
+  private _registerUrl = 'http://18.216.187.147:3000/API/users/register';
+  private _loginUrl = 'http://18.216.187.147:3000/API/users/login';
 
 
   constructor(private http: HttpClient, private _router: Router) { }
